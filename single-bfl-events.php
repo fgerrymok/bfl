1a19c4f4d2f2a6e765a03d5f5910015b1a73bd99
Frazer <fgerrymok@gmail.com>
2024-12-12 18:05:11 -0800

    Added minor css to upcoming events page

--- single-bfl-events.php
+++ single-bfl-events.php
@@ -8,7 +8,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 			?>
-			<h1><?php echo esc_html('Upcoming Events'); ?></h1>
+			<h1 class="single-event-heading"><?php echo esc_html('Upcoming Events'); ?></h1>
 			<?php
 			$hero = get_field('upcoming_events_hero');
 
@@ -19,24 +19,32 @@ get_header();
 				$eventName = $hero['event_name'];
 				$fightDate = $hero['fight_date'];
 				$venue = $hero['venue'];
-				
-				if (empty($bettingOddsLink)) {
-					?>
-					<a href="<?php echo esc_url($bettingOddsLink) ?>"><?php echo esc_html('Betting Odds'); ?></a>
-					<?php
-				}
-
-				if (empty($ticketsLink)) {
-					?>
-					<a href="<?php echo esc_url($ticketsLink) ?>"><?php echo esc_html('Tickets'); ?></a>
-					<?php
-				}
-				
-				echo wp_get_attachment_image($eventsPoster, 'full');
 				?>
-				<h2><?php echo esc_html($eventName); ?></h2>
-				<p><?php echo esc_html($fightDate); ?></p>
-				<p><?php echo esc_html($venue); ?></p>
+				<section class="single-event-hero">
+					<?php
+					if (!empty($bettingOddsLink)) {
+						?>
+						<a href="<?php echo esc_url($bettingOddsLink) ?>" class="betting-odds"><?php echo esc_html('Betting Odds'); ?></a>
+						<?php
+					}
+
+					if (!empty($ticketsLink)) {
+						?>
+						<a href="<?php echo esc_url($ticketsLink) ?>" class="tickets"><?php echo esc_html('Tickets'); ?></a>
+						<?php
+					}
+					?>
+					<div class="event-poster">
+						<?php echo wp_get_attachment_image($eventsPoster, 'full'); ?>
+					</div>
+					<?php
+					?>
+					<div class="fight-details">
+						<h2><?php echo esc_html($eventName); ?></h2>
+						<p><?php echo esc_html($fightDate); ?></p>
+						<p><?php echo esc_html($venue); ?></p>
+					</div>
+				</section>
 				<?php
 			}
 
@@ -49,21 +57,21 @@ get_header();
 					$fighter2Image = $row['fighter_2_image'];
 					$fighter2Profile = $row['fighter_2_profile'];
 					$fightTitle = $row['fight_title'];
-				?>
-				<section class="fight-card">
-					<a href="<?php foreach ($fighter1Profile as $fighter1ProfileId) {
-						echo get_permalink($fighter1ProfileId);
-						} ?>">
-						<?php echo wp_get_attachment_image($fighter1Image, 'full'); ?>
-					</a>
-					<p><?php echo esc_html('vs'); ?></p>
-					<a href="<?php foreach ($fighter2Profile as $fighter2ProfileId) {
-						echo get_permalink($fighter2ProfileId);
-						} ?>">
-						<?php echo wp_get_attachment_image($fighter2Image, 'full'); ?>
-					</a>
-				</section>
-				<?php
+					?>
+					<section class="fight-card">
+						<a href="<?php foreach ($fighter1Profile as $fighter1ProfileId) {
+							echo get_permalink($fighter1ProfileId);
+							} ?>">
+							<?php echo wp_get_attachment_image($fighter1Image, 'full'); ?>
+						</a>
+						<p><?php echo esc_html('vs'); ?></p>
+						<a href="<?php foreach ($fighter2Profile as $fighter2ProfileId) {
+							echo get_permalink($fighter2ProfileId);
+							} ?>">
+							<?php echo wp_get_attachment_image($fighter2Image, 'full'); ?>
+						</a>
+					</section>
+					<?php
 				}
 			}
 			?>
@@ -83,11 +91,19 @@ get_header();
 	
 				$query = new WP_Query($args);
 				if ($query->have_posts()) {
+					?>
+					<section class="single-event-videos">
+					<?php
 					while ($query->have_posts()) {
 						$query->the_post();
 						echo the_content();
 					}
+					?>
+					</section>
+					<?php
 				}
+				
+				wp_reset_postdata();
 			}
 
 		endwhile;
